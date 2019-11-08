707bf51038f0c0735d6056b467d002767dcc04f8
Aurelien Michaud <aurelien.michaud98@gmail.com>
2019-11-08 16:38:42 +0100

    indent

--- src/ftest.ml
+++ src/ftest.ml
@@ -1,44 +1,43 @@
 open Gfile
 open Tools
-    
+
 let testit g =
     let string_g = gmap g int_of_string in 
-        gmap (add_arc string_g 0 3 50) string_of_int 
-        
+    gmap (add_arc string_g 0 3 50) string_of_int 
+
 
 let () =
 
-  (* Check the number of command-line arguments *)
-  if Array.length Sys.argv <> 5 then
+(* Check the number of command-line arguments *)
+    if Array.length Sys.argv <> 5 then
     begin
-      Printf.printf "\nUsage: %s infile source sink outfile\n\n%!" Sys.argv.(0) ;
-      exit 0
+        Printf.printf "\nUsage: %s infile source sink outfile\n\n%!" Sys.argv.(0) ;
+        exit 0
     end ;
 
 
-  (* Arguments are : infile(1) source-id(2) sink-id(3) outfile(4) *)
-  
-  let infile = Sys.argv.(1)
-  and outfile = Sys.argv.(4)
-  
-  (* These command-line arguments are not used for the moment. *)
-  and _source = int_of_string Sys.argv.(2)
-  and _sink = int_of_string Sys.argv.(3)
-  in
+    (* Arguments are : infile(1) source-id(2) sink-id(3) outfile(4) *)
+    let infile = Sys.argv.(1)
+    and outfile = Sys.argv.(4)
 
-  (* Open file *)
-  let graph = from_file infile in
+    (* These command-line arguments are not used for the moment. *)
+    and _source = int_of_string Sys.argv.(2)
+    and _sink = int_of_string Sys.argv.(3)
+    in
 
-  (* TEST CODE HERE *)
+    (* Open file *)
+    let graph = from_file infile in
 
-  (*let new_graph = testit graph in*)
+    (* TEST CODE HERE *)
 
-  (* END OF TEST CODE HERE *)
+    let new_graph = testit graph in
 
-  (* Rewrite the graph that has been read. *)
-  (*let () = write_file outfile new_graph in*)
+    (* END OF TEST CODE HERE *)
 
-  let () = export infile graph in
+    (* Rewrite the graph that has been read. *)
+    (*let () = write_file outfile new_graph in*)
 
-  ()
+    let () = export outfile new_graph in
+
+    ()
 
