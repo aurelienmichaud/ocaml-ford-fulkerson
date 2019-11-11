acac1a7729f07b0d9c981c67ead5d5b48df8edc7
Aurelien Michaud <aurelien.michaud98@gmail.com>
2019-11-11 12:20:48 +0100

    ford-fulkerson and side functions

--- src/flownetwork.ml
+++ src/flownetwork.ml
@@ -1,4 +1,5 @@
 open Graph
+open Tools
 
 type flow = int
 type capacity = int
@@ -7,86 +8,6 @@ type edge = flow * capacity
 
 type flownetwork = edge graph
 
-(* Get all the outcoming nodes of node 'id', 
- * with the following condition : flow < capacity *)
-let get_out_nodes fn id = 
-    List.fold_left 
-        (fun acc (_id, (_flow, _capacity)) -> if _flow < _capacity then _id::acc else acc) 
-        []
-        (out_arcs fn id)
-
-(* Get all the outcoming arcs of node 'id', 
- * with the following condition : flow < capacity *)
-let get_out_arcs fn id =
-    List.fold_left 
-        (fun acc ((_id, (_flow, _capacity)) as arc) -> if _flow < _capacity then arc::acc else acc) 
-        []
-        (out_arcs fn id)
-
-(* Get all the outcoming arcs of node 'id', 
- * with the following condition : flow < capacity
- * but we only return the id of the node AND the gap
- * capacity - flow *)
-let get_out_arcs_gaps fn id =
-    List.fold_left 
-        (fun acc (_id, (_flow, _capacity)) -> if _flow < _capacity then (_id, _capacity - _flow)::acc else acc) 
-        []
-        (out_arcs fn id)
-
-
-
-
-
-
-
-(* Exception raised when in find_augmenting_path we found the ending node
- * which means we found an augmenting path.
- * We use an exception in order to stop the entire search algorithm *)
-exception Found_Augmenting_Path of ((id list) * int)
-
-(* Find one augmenting path,
- * i.e. a path in graph fn from node 's' to node 'e'
- * where all the edges verify the following condition :
- * flow < capacity *)
-let find_augmenting_path fn s e = 
-    (* Depth First Search *)
-    let rec dfs acc delta visited = function
-    | []            -> visited
-    | (id, gap)::t  -> 
-        (* We found the node we were looking for, so we stop the loop by raising an exception *)
-        if id = e then
-            let delta = if gap < delta || delta = 0 then gap else delta in
-            raise (Found_Augmenting_Path (List.rev (e::acc), delta))
-
-        (* We already visited that node, so we ignore it and parse the other out_arcs provided in the list *)
-        else if List.mem id visited then
-            dfs acc delta visited t
-
-        (* We need to parse the node id first, since we use Depth First Search (DFS) *)
-        else
-            let new_visited = 
-                if gap < delta || delta = 0 then
-                    dfs (id::acc) gap (id::visited) (get_out_arcs_gaps fn id)
-                else
-                    dfs (id::acc) delta (id::visited) (get_out_arcs_gaps fn id)
-            in
-            dfs acc delta new_visited t
-        
-    in
-    dfs [s] 0 [s] (get_out_arcs_gaps fn s)
-
-
-let update_edge fn id1 id2 delta =
-    match find_arc fn id1 id2 with 
-    | None  -> fn
-    | Some (flow, capacity) -> new_arc fn id1 id2 (flow + delta, capacity)
-
-let rec update_flow_network fn delta = function
-    | []            -> fn
-    | [id]          -> fn
-    (* We cannot use add_arc, since it takes an int graph, and
-     * a flow network is a (flow * capacity) graph *)
-    | id1::(id2::t as tail)   -> update_flow_network (update_edge fn id1 id2 delta) delta tail
 
 
 
@@ -114,27 +35,135 @@ let update fn id1 id2 flow =
             
 *)
 
+type direction = Same | Opposite
+type residual_graph = (flow * direction) graph
+
+let get_residual_graph fn = 
+    let cloned = clone_nodes fn in
+    e_fold
+    fn
+    (fun residual_graph id1 id2 (flow, capacity) ->
+        if capacity <> 0 then
+        begin
+            if flow <> 0 && flow <> capacity then
+                let new_residual_graph = new_arc residual_graph id1 id2 (capacity - flow, Same) in
+                new_arc new_residual_graph id2 id1 (flow, Opposite) 
+            else if flow = 0 then
+                new_arc residual_graph id1 id2 (capacity, Same)
+            else
+                new_arc residual_graph id2 id1 (capacity, Opposite)
+        end
+        else
+            residual_graph)
+    cloned
 
 
+exception Found_Augmenting_Path of (((id * direction) list) * int)
 
-(* fn : flownetwork *)
-(* s: Start node, the node we begin our research from
- * e: End node, the node we are looking for
+(* Find one augmenting path in a residual graph
+ * It returns the found path, and the bottleneck capacity (delta)
+ * by which we will increase the flow of each arc described in the path
+ * The returning path is shaped as follow :
+ * path : (id, direction) list
+ * For instance : path = [(sc, Same); (3, Opposite); (7, Same); (sk, Same)]
+ * which means we will need (in ford-fulkerson algorithm) to update 
+ * the following arcs :
+ * (sc, 3) with +delta; (3, 7) with -delta; (7, sk) with +delta
+ * So 'Same direction means we will increase by +delta, 
+ * and 'Opposite' direction by -delta.
+ * The direction of the first pair (id, direction) doesn't matter
  *)
+let find_augmenting_path rg sc sk = 
+    (* Depth First Search *)
+    let rec dfs acc delta visited = function
+        | []            -> visited
+        | (id, (residual_flow, direction))::t  -> 
+            (* We found the node we were looking for, so we stop the loop by raising an exception *)
+            if id = sk then
+                let delta = if residual_flow < delta || delta = 0 then residual_flow else delta in
+                raise (Found_Augmenting_Path (List.rev ((sk, direction)::acc), delta))
 
-let ford_fulkerson fn s e = 
+            (* We already visited that node, so we ignore it and parse the other out_arcs provided in the list *)
+            else if List.mem id visited then
+                dfs acc delta visited t
+
+            (* We need to parse the node id first, since we use Depth First Search (DFS) *)
+            else
+                let new_visited =
+                    if residual_flow < delta || delta = 0 then
+                        dfs ((id, direction)::acc) residual_flow (id::visited) (out_arcs rg id)
+                    else
+                        dfs ((id, direction)::acc) delta (id::visited) (out_arcs rg id)
+                in
+                dfs acc delta new_visited t
+        
+    in
+    dfs [(sc, Same)] 0 [sc] (out_arcs rg sc)
+
+let update_edge fn id1 id2 delta =
+    match find_arc fn id1 id2 with 
+    | None  -> fn
+    | Some (flow, capacity) -> new_arc fn id1 id2 (flow + delta, capacity)
+
+(* This function receives the path '(id, direction) list'
+ * resulting from 'find_augmenting_path' *)
+let rec update_flow_network fn delta = function
+    | []                -> fn
+    | [(id, direction)] -> fn
+    (* We cannot use add_arc, since it takes an int graph, and
+     * a flow network is a (flow * capacity) graph *)
+    | (id1, dir1)::((id2, dir2)::t as tail)   ->    
+        match dir2 with
+        | Same      -> update_flow_network (update_edge fn id1 id2 delta) delta tail
+        | Opposite  -> update_flow_network (update_edge fn id1 id2 (-delta)) delta tail
+
+
+(* Ford-Fulkerson Algorithm on flow networks
+
+ * fn : flownetwork
+ * sc: Source node, the node we begin our research from
+ * sk: Sink node, the node we are looking for
+
+ * Principle of the algorithm :
+ 
+ * While we can find an augmenting path
+ *      - Find an augmenting path 
+ *      - Get the bottleneck capacity (basically the delta by which we can increase the flow in the augmenting path) 
+ *      - Update the flow network with that bottleneck capacity in the augmenting path
+
+ *)
+let ford_fulkerson fn sc sk = 
     (* Check whether the start node or the end node does not exist *)
-    if (not (node_exists fn s)) || (not (node_exists fn e)) then
-        raise (Graph_error ("Start node and/or End node do/es not exist in flow network"))
+    if (not (node_exists fn sc)) || (not (node_exists fn sk)) then
+        raise (Graph_error ("Source node and/or Sink node do/es not exist in flow network"))
     else
     
     (* TEST 
      * Find one augmenting path, update the flow network with the delta found
      * in that augmented path, and return that new flow network *)
+    (*
     try
-        let _ = find_augmenting_path fn s e in
+        let _ = find_augmenting_path fn sc sk in
         fn
     with
         (Found_Augmenting_Path (path, delta))   -> update_flow_network fn delta path
+    *)
+
+    
+    let rec loop fn = 
+    
+        let residual_fn = get_residual_graph fn in
+
+        try
+            let _ = find_augmenting_path residual_fn sc sk in
+            (* If no exception is raised, then we finished the algorithm *)
+            fn
+        with (Found_Augmenting_Path (path, delta))  -> loop (update_flow_network fn delta path)
+    in
+    loop fn
+
+    
+
+
 
 
