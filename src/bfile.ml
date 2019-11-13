b48275c4cfe96e532b33c71485bb7734c12a37f9
Aurelien Michaud <aurelien.michaud98@gmail.com>
2019-11-13 13:22:01 +0100

    bfile read_capacity

--- src/bfile.ml
+++ src/bfile.ml
@@ -6,25 +6,28 @@ type path = string
 
 let read_capacity_line n graph hashtbl name capacity = 
   
-  (* If the name is already in the Hashtbl, we update it 
-   * If not, we add it to the hashtbl *)
-  let id_name =
-    if Hashtbl.mem hashtbl name then Hashtbl.find hashtbl name
-    else Hashtbl.add hashtbl name n; n
-  in
+    (* If the name is already in the Hashtbl, we update it 
+     * If not, we add it to the hashtbl *)
+    let name_id =
+        if Hashtbl.mem hashtbl name then Hashtbl.find hashtbl name
+        else Hashtbl.add hashtbl name n; n
+    in
 
-  let gr =
-    new_arc graph id_name 999 (0, capacity)
-  in
-  gr
+    let sink_id = Hashtbl.find "__sink__" in
 
+    new_arc graph name_id sink_id (0, int_of_string capacity)
+
+(* weight is not used for now *)
 let read_connection_line n graph hashtbl weight froms tos = 
+
+      
+     
 
 
 (* NameX[,NameY,...]=NameJ[,NameK,...]*)
 let get_connection_line line =
     match String.split_on_char '=' line with
-        | [froms;tos]   -> Some (froms, tos)
+        | [froms;tos]   -> Some (String.trim froms, String.trim tos)
         | _             -> None
 
 (* Weight|NameX[,NameY,...]=NameJ[,NameK,...]*)
@@ -33,7 +36,7 @@ let get_weighed_connection_line line =
         | [weight;rest] -> 
             begin
             match get_connection_line rest with
-                | Some (froms, tos) -> Some (weight, froms, tos)
+                | Some (froms, tos) -> Some (String.trim weight, String.trim froms, String.trim tos)
                 | _ -> None
             end
         | _ -> None
@@ -43,7 +46,7 @@ let get_weighed_connection_line line =
  * University1:20 *)
 let get_capacity_line line = 
     match String.split_on_char ':' line with
-        | [name;capacity]   -> Some (name, capacity)
+        | [name;capacity]   -> Some (String.trim name, String.trim capacity)
         | _                 -> None
 
 let read_line n graph hashtbl line =
@@ -67,6 +70,13 @@ let from_file path =
 
     let hashtbl = Hashtbl.create 100 in
 
+    (* Add source and sink nodes in the flow_network *)
+    let base_graph = new_node empty_graph 9998 in
+    let base_graph = new_node base_graph 9999 in
+    (* Add source and sink nodes in the hash table *)
+    hashtbl.add hashtbl "__source__" 9998;
+    hashtbl.add hashtbl "__sink__" 9999;
+
     let infile = open_in path in
     
     let rec loop n graph =
@@ -88,8 +98,6 @@ let from_file path =
         with End_of_file    -> graph
     in
     
-    let base_graph = new_node empty_graph 998 in
-    let base_graph = new_node base_graph 999 in
 
     let final_graph = loop 0 base_graph in
     
