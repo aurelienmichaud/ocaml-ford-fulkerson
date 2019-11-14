15f3cba5547cf0fdc99925f6464eea0bf4ea6b59
Aurelien Michaud <aurelien.michaud98@gmail.com>
2019-11-14 17:24:55 +0100

    weighed flow network for bipartite done

---
+++ _build/src/bipartite.mli
@@ -0,0 +1,5 @@
+(*open Flownetwork*)
+open Weighedflownetwork
+
+val solve: weighedflownetwork -> weighedflownetwork
+
---
+++ _build/src/weighedflownetwork.mli
@@ -0,0 +1,13 @@
+open Graph
+
+type flow       = int
+type capacity   = int
+type weight     = int
+
+type edge = flow * capacity * weight
+
+type weighedflownetwork = edge graph
+
+(*val busacker_gowen: weighedflownetwork -> id -> id -> weighedflownetwork*)
+val ford_fulkerson: weighedflownetwork -> id -> id -> weighedflownetwork
+
